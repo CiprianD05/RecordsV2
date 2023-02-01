9850949338cd03fe18513ba6abc3cae409a961de
CiprianD05 <diaconu.c.ion05@gmial.com>
2023-02-01 17:37:43 +0200

    Added VC functionality

---
+++ Records/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace Record.Controllers
+{
+    //[Authorize(Roles = "User")]
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}

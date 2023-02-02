adbaddd3fc9564ddcaa82d1faf16138677b6edc9
CiprianD05 <diaconu.c.ion05@gmial.com>
2023-02-02 11:49:36 +0200

    Added connection strings to app.json and tested

--- Records_DbLibrary/RecordsDbContext.cs
+++ Records_DbLibrary/RecordsDbContext.cs
@@ -1,7 +1,40 @@
-﻿namespace Records_DbLibrary
+﻿using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+
+namespace DbLibrary
 {
-    public class RecordsDbContext
+    public class RecordDbContext : DbContext
     {
+
+        private static IConfigurationRoot _configuration;
+
+        
+        public RecordDbContext()
+        {
+
+        }
+
+        public RecordDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("Records");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+
 
     }
 }

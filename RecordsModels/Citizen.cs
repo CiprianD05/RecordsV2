f5d31470f27304e6b682095159d74fb83238be56
CiprianD05 <diaconu.c.ion05@gmial.com>
2023-02-02 15:20:41 +0200

    Added Models Project and CItizen Class

---
+++ RecordsModels/Citizen.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordsModels
+{
+    public class Citizen
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string? SocialSecurityNumber { get; set; }
+
+        public string? PassportNumber { get; set; }
+
+    }
+}

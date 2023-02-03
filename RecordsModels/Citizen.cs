b8fc1b147a8aa4fc0bdc4a369f8273803d80d531
CiprianD05 <diaconu.c.ion05@gmial.com>
2023-02-03 10:40:02 +0200

    Used Data Annotoations on Citizen class

--- RecordsModels/Citizen.cs
+++ RecordsModels/Citizen.cs
@@ -1,12 +1,21 @@
-﻿namespace RecordsModels
+﻿using System.ComponentModel.DataAnnotations;
+namespace RecordsModels
 {
     public class Citizen
     {
+        [Key]
+        [Required]        
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
 
-        public string? SocialSecurityNumber { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string SocialSecurityNumber { get; set; }
 
+        [MaxLength(100)]
         public string? PassportNumber { get; set; }
 
     }

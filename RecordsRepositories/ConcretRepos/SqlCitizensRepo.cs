7d0a454a4c5ea4f58bfda3769f5bb0960f97d782
CiprianD05 <diaconu.c.ion05@gmial.com>
2023-02-03 13:55:45 +0200

    Added base CitizensRepo

---
+++ RecordsRepositories/ConcretRepos/SqlCitizensRepo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecordsModels;
+using RecordsRepositories.Interfaces;
+
+namespace RecordsRepositories.ConcretRepos
+{
+    public class SqlCitizensRepo : ICitizenRepo
+    {
+        public void CreateCitizen(Citizen citizen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCitizen(Citizen citizen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Citizen GetAllCitizenById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Citizen> GetAllCitizens()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCitizen(Citizen citizen)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

fba8b67cdc47c26547f4c09e39948afb061b2134
CiprianD05 <diaconu.c.ion05@gmial.com>
2023-04-19 20:45:55 +0300

    Added DocumentType SQL repository

---
+++ RecordsRepositories/ConcretRepos/SqlDocumentTypeRepo.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordsDbLibrary;
+using RecordsModels;
+using RecordsRepositories.Interfaces;
+using RecordsDbLibrary;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace RecordsRepositories.ConcretRepos
+{
+    public class SqlDocumentTypeRepo : IDocumentTypeRepo
+    {
+        private RecordsDbContext _context;
+
+        public SqlDocumentTypeRepo(RecordsDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<DocumentType> CreateDocumentType(DocumentType documentType)
+        {
+            if(documentType==null)
+                throw new ArgumentNullException(nameof(documentType));
+
+            EntityEntry<DocumentType> entityEntry = await _context.AddAsync(documentType);
+
+            return entityEntry.Entity;
+        }
+
+        public void DeleteDocumentType(DocumentType documentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DocumentType> GetAllDocumentTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DocumentType> GetAllDocumentTypesById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDocumentType(DocumentType documentType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RecordsRepositories/Interfaces/IDocumentTypeRepo.cs
@@ -0,0 +1,14 @@
+﻿using RecordsModels;
+
+namespace RecordsRepositories.Interfaces
+{
+    interface IDocumentTypeRepo
+    {
+        Task<bool> SaveChanges();
+        IEnumerable<DocumentType> GetAllDocumentTypes();
+        Task<DocumentType> GetAllDocumentTypesById(int Id);
+        Task<DocumentType> CreateDocumentType(DocumentType documentType);
+        void UpdateDocumentType(DocumentType documentType);
+        void DeleteDocumentType(DocumentType documentType);
+    }
+}
